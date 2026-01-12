102a6d8e753ed3f3a3ab3844a5d2344690398975
Usman <abc.usman.100@gmail.com>
2026-01-12 12:16:12 +0500

    Login and logout DONE

--- Routers/UserRouter.js
+++ Routers/UserRouter.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const {RegisterUser} = require('../Controllers/AuthController');
+const {RegisterUser, LoginUser, LogoutUser} = require('../Controllers/AuthController');
 
 router.post('/register', RegisterUser);
+router.post('/login', LoginUser);
+router.post('/logout', LogoutUser);
 
 
 module.exports = router;
